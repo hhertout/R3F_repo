aba6b98779a68f87d0a0d10d0edd87b2d06e525b
hhertout <hugues.hertout@gmail.com>
2023-10-20 19:57:34 +0200

    chore: install husky and prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,22 @@
 'use client'
-import {Canvas} from "@react-three/fiber";
-import {OrbitControls} from "@react-three/drei";
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+import { StrictMode } from 'react'
 
 export default function Home() {
   return (
-    <div id="canvas-container" style={{height: "100vh", width: "100vw"}}>
-      <Canvas>
-        <ambientLight intensity={0.1}/>
-        <directionalLight color="#FF2345" position={[1, 2, 5]}/>
-        <mesh>
-          <boxGeometry args={[2, 2, 2]}/>
-          <meshStandardMaterial/>
-        </mesh>
-        <OrbitControls makeDefault/>
-      </Canvas>
+    <div id="canvas-container" style={{ height: '100vh', width: '100vw' }}>
+      <StrictMode>
+        <Canvas>
+          <ambientLight intensity={0.1} />
+          <directionalLight color="#FF2345" position={[1, 2, 5]} />
+          <mesh>
+            <boxGeometry args={[2, 2, 2]} />
+            <meshStandardMaterial />
+          </mesh>
+          <OrbitControls makeDefault />
+        </Canvas>
+      </StrictMode>
     </div>
   )
 }
