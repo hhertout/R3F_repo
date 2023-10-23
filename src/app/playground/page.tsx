719b9559144073d3e26eb5abf504ce214090ee95
hhertout <hugues.hertout@gmail.com>
2023-10-23 22:37:25 +0200

    feat: isolate css of playground and implement of leva to debug 3D objects

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -4,16 +4,18 @@ import React, { StrictMode } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { Html, OrbitControls } from '@react-three/drei';
 import OrganicSphere from '@components/experience/OrganicSphere';
+import { useControls } from 'leva';
+import './playground.css';
 
 const Page = () => {
+  const { position } = useControls({ position: [-3, 0.2, 0] });
   return (
     <body>
       <div id="canvas-container">
-        <Canvas>
+        <Canvas gl={{ alpha: true }}>
           <StrictMode>
-            <color attach={'background'} args={[0, 0, 0]} />
             <ambientLight />
-            <OrganicSphere radius={2} depth={120} position={[-3, 0.2, 0]} />
+            <OrganicSphere radius={2} depth={120} position={position} />
             <Html center>
               <h1 style={{ color: 'white', width: '100%' }}>
                 Organic&#160;Sphere
